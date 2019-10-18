b3db47a695451a987daa7cb0ef8d39f352762b0b
charchit95 <47504288+charchit95@users.noreply.github.com>
2019-10-18 16:59:44 +0530

    Update and rename tester.cpp to tester.c

---
+++ tester.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    int j=17;
+    int k=8;
+    int h=9;
+    clrscr();
+    
+    a=j+k+h;
+    printf("%d",a);
+    
+  getch();
+   //hi
+    //ki
+    ///olololloll
+
+}
